37930f2956a8a1a50d77091e4831ee4412a10002
solerem <maxime.solere@gmail.com>
2025-07-12 16:24:52 -0400

    Basic info structure

---
+++ data.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import yfinance as yf
+
+
+class Data:
+
+    period = '5y'
+
+    def __init__(self, currency, etf_list, etf_preference):
+
+        self.sgd_rate = None
+        self.nav = None
+        self.returns = None
+
+        self.etf_list = etf_list
+        self.currency = currency
+        self.get_sgd_rate()
+        self.get_rf_rate()
+        self.get_nav()
+        self.returns = self.nav.pct_change().iloc[1:]
+
+
+    def get_sgd_rate(self):
+
+        if self.currency == 'SGD':
+            self.sgd_rate = 1
+
+        else:
+            ticker = yf.Ticker(f'{self.currency}SGD=X')
+            self.sgd_rate = ticker.history(period='1d', interval='1h')['Close'].iloc[-1]
+
+
+    def get_rf_rate(self):
+
+        irx = yf.download('^IRX', period=Data.period, interval='1mo', auto_adjust=True)['Close']
+        self.rf_rate = (irx/100) ** (1/12)
+
+
+    def get_nav(self):
+
+        self.nav = yf.download(self.etf_list, period=Data.period, interval='1mo', auto_adjust=True)['Close']
+        self.add_BTC()
+
+
+    def add_BTC(self):
+
+        btc = yf.download('BTC-USD', period=Data.period, interval='1mo', auto_adjust=True)['Close']['BTC-USD']
+
+        if self.currency != 'USD':
+            ticker = f'USD{self.currency}=X'
+            rate = yf.download(ticker, period='5y', interval='1mo', auto_adjust=True)['Close'][ticker]
+            btc *= rate
+
+        self.nav['BTC'] = btc
+
+
---
+++ info.py
@@ -0,0 +1,72 @@
+from data import Data
+
+
+class Info:
+
+
+    etf_list = {
+        'SGD': ['VOO'],
+        'EUR': ['VOO'],
+        'USD': ['VOO'],
+    }
+
+    etf_preference = {
+        'SGD': {},
+        'EUR': {},
+        'USD': {}
+    }
+
+    currency_config = {
+        1: 'SGD',
+        2: 'EUR',
+        3: 'USD'
+    }
+
+
+    def __init__(self, risk, cash_sgd, holdings, currency, allow_short):
+
+        self.risk = risk
+        self.cash_sgd = cash_sgd
+        self.holdings = holdings if holdings else {}
+        self.allow_short = allow_short
+        self.currency = currency if currency else Info.currency_config[self.risk]
+        self.etf_list = Info.etf_list[self.currency]
+        self.etf_preference = Info.etf_preference[self.currency]
+        self.transform_etf_preference()
+
+
+    def transform_etf_preference(self):
+
+        temp = {}
+        for preferred in self.etf_preference:
+            temp[preferred] = True
+            temp[self.etf_preference[preferred]] = False
+
+        self.etf_preference = temp
+
+
+
+
+class Portfolio(Info):
+
+
+    def __init__(self, risk=3, cash_sgd=100, holdings=None, currency=None, allow_short=False):
+
+        super().__init__(risk, cash_sgd, holdings, currency, allow_short)
+
+        self.data = Data(self.currency, self.etf_list, self.etf_preference)
+        self.cash = self.cash_sgd / self.data.sgd_rate
+        print(self.cash)
+
+
+
+
+
+Portfolio()
+
+
+
+
+
+
+

4112c117bd514c47e85d7f70abff8aadb2e5233a
solerem <maxime.solere@gmail.com>
2025-08-30 02:25:16 +0800

    fix ReadTheDocs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath(".."))
+import os, sys
+sys.path.insert(0, os.path.abspath(".."))        # repo root
+sys.path.insert(0, os.path.abspath("../code"))   # your source folder (adjust if different)
+
 
 
 project = 'ETF Portfolio Optimization'
